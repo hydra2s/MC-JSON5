d82440dcef1d0e39fca9f684c20ebce8f69226fc
unit-a-user <helixd.2s18@gmail.com>
2022-11-16 15:06:01 +0800

    try and catch

---
+++ src/main/java/net/hydra2s/mc_json5/mixin/JSON5Getter.java
@@ -0,0 +1,8 @@
+package net.hydra2s.mc_json5.mixin;
+
+import com.google.gson.JsonElement;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(JsonElement.class)
+public class JSON5Getter {
+}
--- src/main/java/net/hydra2s/mc_json5/mixin/JSON5Mixin.java
+++ src/main/java/net/hydra2s/mc_json5/mixin/JSON5Mixin.java
@@ -4,11 +4,14 @@ import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
+import com.mojang.logging.LogUtils;
 import de.marhali.json5.Json5;
+import de.marhali.json5.exception.Json5Exception;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.JsonHelper;
 import net.minecraft.util.registry.Registries;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.annotation.Contract;
 import org.spongepowered.asm.mixin.Mixin;
@@ -24,25 +27,64 @@ import java.io.StringWriter;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.*;
+import org.slf4j.Logger;
 
 @Mixin(JsonHelper.class)
 public class JSON5Mixin {
-    @Unique private static final Json5 JSON5Reader = Json5.builder(options -> options.allowInvalidSurrogate().quoteSingle().prettyPrinting().build());
-    @Unique private static final Json5 JSON5Writer = Json5.builder(options -> options.build());
+    @Unique private static final Json5 JSON5Reader = Json5.builder(options -> options.allowInvalidSurrogate().quoteSingle().trailingComma().build());
+    @Unique private static final Json5 JSON5Writer = Json5.builder(options -> options.prettyPrinting().build());
+
+    //
+    private static final Logger LOGGER = LogUtils.getLogger();
 
     /**
      * @author
      * @reason
      */
-    @Overwrite @Nullable
-    public static <T> T deserialize(Gson gson, Reader reader, Class<T> type, boolean lenient) {
+    @Overwrite(remap = true) @Nullable
+    public static <T> T deserialize(Gson gson, Reader reader, Class<T> type, boolean lenient) throws IOException {
+        var json5 = IOUtils.toString(reader);
+
+        // if still malformed, try parse as JSON5
+        if (lenient) {
+            try {
+                json5 = JSON5Writer.serialize(JSON5Reader.parse(json5));
+            } catch (IOException var7) {
+                throw new Json5Exception(var7);
+            }
+
+            //
+            LOGGER.info("DEBUG JSON: ");
+            LOGGER.info(json5);
+        }
+
+        //
         try {
-            Reader reader_ = lenient ? new StringReader(JSON5Writer.serialize(JSON5Reader.parse(reader))) : reader;
-            JsonReader jsonReader = new JsonReader(reader_);
+            // if still malformed, try parse as JSON5
+            JsonReader jsonReader = new JsonReader(new StringReader(json5));
             jsonReader.setLenient(true);
             return gson.getAdapter(type).read(jsonReader);
         } catch (IOException var5) {
-            throw new JsonParseException(var5);
+            try {
+                // if still malformed, try parse as JSON5
+                try {
+                    json5 = JSON5Writer.serialize(JSON5Reader.parse(json5));
+                } catch(IOException var7) {
+                    throw new Json5Exception(var7);
+                }
+
+                //
+                LOGGER.info("DEBUG JSON: ");
+                LOGGER.info(json5);
+
+                // if still malformed, try parse as JSON5
+                JsonReader jsonReader = new JsonReader(new StringReader(json5));
+                jsonReader.setLenient(true);
+                return gson.getAdapter(type).read(jsonReader);
+            } catch (IOException var6) {
+                throw new JsonParseException(var6);
+            }
+            //throw new JsonParseException(var5);
         }
     }
 
@@ -50,15 +92,50 @@ public class JSON5Mixin {
      * @author
      * @reason
      */
-    @Overwrite @Nullable
-    public static <T> T deserialize(Gson gson, Reader reader, TypeToken<T> typeToken, boolean lenient) {
+    @Overwrite(remap = true) @Nullable
+    public static <T> T deserialize(Gson gson, Reader reader, TypeToken<T> typeToken, boolean lenient) throws IOException {
+        var json5 = IOUtils.toString(reader);
+
+        // if still malformed, try parse as JSON5
+        if (lenient) {
+            try {
+                json5 = JSON5Writer.serialize(JSON5Reader.parse(json5));
+            } catch (IOException var7) {
+                throw new Json5Exception(var7);
+            }
+
+            //
+            LOGGER.info("DEBUG JSON: ");
+            LOGGER.info(json5);
+        }
+
+        //
         try {
-            Reader reader_ = lenient ? new StringReader(JSON5Writer.serialize(JSON5Reader.parse(reader))) : reader;
-            JsonReader jsonReader = new JsonReader(reader_);
+            // if still malformed, try parse as JSON5
+            JsonReader jsonReader = new JsonReader(new StringReader(json5));
             jsonReader.setLenient(true);
             return gson.getAdapter(typeToken).read(jsonReader);
         } catch (IOException var5) {
-            throw new JsonParseException(var5);
+            try {
+                // if still malformed, try parse as JSON5
+                try {
+                    json5 = JSON5Writer.serialize(JSON5Reader.parse(json5));
+                } catch(IOException var7) {
+                    throw new Json5Exception(var7);
+                }
+
+                //
+                LOGGER.info("DEBUG JSON: ");
+                LOGGER.info(json5);
+
+                // if still malformed, try parse as JSON5
+                JsonReader jsonReader = new JsonReader(new StringReader(json5));
+                jsonReader.setLenient(true);
+                return gson.getAdapter(typeToken).read(jsonReader);
+            } catch (IOException var6) {
+                throw new JsonParseException(var6);
+            }
+            //throw new JsonParseException(var5);
         }
     }
 }
